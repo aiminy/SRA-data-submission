41531aa333e4ece93cf8b645df9dd8c0504ba1d1
aiminy <jlfmssm@gmail.com>
2017-06-20 16:36:55 -0400

    add aspera.openssh.file

--- R/CallBash.R
+++ R/CallBash.R
@@ -8,19 +8,21 @@
 #' @examples
 #' 
 #' input.bam.file.path <- "/media/H_driver/2016/Submission2SRA/Guoyan_Nimer_mouse_RNA-Seq_BAM/"
+#' aspera.openssh.file <- "/media/H_driver/2016/Submission2SRA/Christopher Williams_mouse_RNA-Seq_BAM/aspera.openssh"
 #' 
 #' callSubmitter(input.bam.file.path)
 #' 
 #' 
-callSubmitter <- function(input.bam.file.path) {
+callSubmitter <- function(input.bam.file.path,aspera.openssh.file) {
   
   R_LIB=.libPaths()
   
   cmd0 <- file.path(R_LIB,"SraSubmitter/bin/bash/bash_SRA_submission_updated.sh")
   
   input.bam.file.path <- input.bam.file.path
+  aspera.openssh.file <- aspera.openssh.file
   
-  cmd1=paste("sh",cmd0,input.bam.file.path,sep=" ")
+  cmd1=paste("sh",cmd0,input.bam.file.path,aspera.openssh.file,sep=" ")
   
   system(cmd1)
 
--- inst/bin/bash/bash_SRA_submission_updated.sh
+++ inst/bin/bash/bash_SRA_submission_updated.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #Usage: sh /home/aiminyan/Code/bash_SRA_submission_updated.sh input_files_dir
-#Example: sh /home/aiminyan/Code/bash_SRA_submission_updated.sh /media/H_driver/2016/Submission2SRA/Guoyan_Nimer_mouse_RNA-Seq_BAM/
+#Example: sh /home/aiminyan/Code/bash_SRA_submission_updated.sh /media/H_driver/2016/Submission2SRA/Guoyan_Nimer_mouse_RNA-Seq_BAM/ "/media/H_driver/2016/Submission2SRA/Christopher\ Williams_mouse_RNA-Seq_BAM/aspera.openssh"
 
 DIR="$1"
+aspera_openssh_file="$2"
 
 for file in $(ls $DIR*.bam)
 do
@@ -18,6 +19,6 @@ sample_name=$(echo "$file_name" | awk -F"." '{print $1}')
 
 echo "$dir_name"/"$file_name"
 
-/home/aiminyan/.aspera/connect/bin/ascp -i /media/H_driver/2016/Submission2SRA/Christopher\ Williams_mouse_RNA-Seq_BAM/aspera.openssh -QT -l100m -k1 -d "$dir_name"/"$file_name" subasp@upload.ncbi.nlm.nih.gov:uploads/jlfmssm@gmail.com_lTVET0In/MTG/
+/home/aiminyan/.aspera/connect/bin/ascp -i "$aspera_openssh_file" -QT -l100m -k1 -d "$dir_name"/"$file_name" subasp@upload.ncbi.nlm.nih.gov:uploads/jlfmssm@gmail.com_lTVET0In/MTG/
 
 done
